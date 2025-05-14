bb68b03958c15ef7e298187194b30e319ee646ff
Kirill <krlsrlsv@gmail.com>
2025-05-14 13:50:11 +0200

    Created a simple UI for the login to test the Backend

--- HabitHuddle-iOS/ContentView.swift
+++ HabitHuddle-iOS/ContentView.swift
@@ -9,47 +9,27 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
+    
+    @State private var email: String = ""
+    @State private var password: String = ""
 
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
+        VStack {
+            VStack {
+                TextField(text: $email) {
+                    Text("Email")
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
+                TextField(text: $password) {
+                    Text("Password")
                 }
             }
-        } detail: {
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
+            .padding(.horizontal)
+            Button {
+                print("Sending data...")
+                print("Email: \(email)")
+                print("Password: \(password)")
+            } label: {
+                Text("Register")
             }
         }
     }
@@ -57,5 +37,4 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
 }
