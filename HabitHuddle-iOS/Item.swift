8bddf650edd1a8a7ba77641bb98bd383ed44b7c2
Kirill <krlsrlsv@gmail.com>
2025-05-14 13:36:15 +0200

    Initial Commit

---
+++ HabitHuddle-iOS/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  HabitHuddle-iOS
+//
+//  Created by Kirill on 14.05.25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ HabitHuddle-iOSTests/HabitHuddle_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabitHuddle_iOSTests.swift
+//  HabitHuddle-iOSTests
+//
+//  Created by Kirill on 14.05.25.
+//
+
+import Testing
+@testable import HabitHuddle_iOS
+
+struct HabitHuddle_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
