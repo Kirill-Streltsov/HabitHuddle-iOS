0135ec3da327bcaf6327c4260d43627014edceb0
Kirill <krlsrlsv@gmail.com>
2025-05-19 14:08:15 +0200

    Removed print from LoginView

--- HabitHuddle-iOS/Core/Authentication/LoginView.swift
+++ HabitHuddle-iOS/Core/Authentication/LoginView.swift
@@ -35,7 +35,6 @@ struct LoginView: View {
                 .padding(12)
 
                 Button {
-                    print("Log user in...")
                 } label: {
                     HStack {
                         Text("SIGN IN")
