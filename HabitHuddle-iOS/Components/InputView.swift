6bb28e153dbc3b4518972b69d4594a7e97c87ad3
Kirill <krlsrlsv@gmail.com>
2025-05-20 11:02:12 +0200

    Removed a tab from InputView

--- HabitHuddle-iOS/Components/InputView.swift
+++ HabitHuddle-iOS/Components/InputView.swift
@@ -26,7 +26,6 @@ struct InputView: View {
                 TextField(placeholder, text: $text)
                     .font(.system(size: 14))
             }
-
             Divider()
         }
     }
