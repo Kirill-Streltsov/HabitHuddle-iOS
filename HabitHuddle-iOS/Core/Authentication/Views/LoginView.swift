50fd61b5af57a06308af0ad337971f3da4936e20
Kirill <krlsrlsv@gmail.com>
2025-05-28 23:05:22 +0200

    Commented saving the user

--- HabitHuddle-iOS/Core/Authentication/Views/LoginView.swift
+++ HabitHuddle-iOS/Core/Authentication/Views/LoginView.swift
@@ -60,7 +60,7 @@ struct LoginView: View {
                                     )
                                     context.insert(habit)
                                 }
-                                viewModel.saveUser(cu, using: context)
+                                //viewModel.saveUser(cu, using: context)
                             } onFailure: { apiError in
                                 print("Couldn't load user habits: \(apiError.localizedDescription)")
                             }
