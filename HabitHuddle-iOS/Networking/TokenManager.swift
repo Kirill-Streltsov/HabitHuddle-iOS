4d134c2fb10a33afbcaa4bc2d2fb0c0607938386
Kirill <krlsrlsv@gmail.com>
2025-05-29 22:20:58 +0200

    Implemented KeyChain for storing the user token

--- HabitHuddle-iOS/Networking/TokenManager.swift
+++ HabitHuddle-iOS/Networking/TokenManager.swift
@@ -6,26 +6,57 @@
 //
 
 import Foundation
+import Security
 
 enum TokenManager {
-    private enum Keys {
-        static let token = "authToken"
-    }
+    private static let tokenKey = "authToken"
+    private static let service = "com.krlsrlsv.HabitHuddle-iOS"
 
     static var token: String? {
         get {
-            UserDefaults.standard.string(forKey: Keys.token)
-        }
-        set {
-            if let token = newValue {
-                UserDefaults.standard.set(token, forKey: Keys.token)
-            } else {
-                UserDefaults.standard.removeObject(forKey: Keys.token)
+            let query: [String: Any] = [
+                kSecClass as String: kSecClassGenericPassword,
+                kSecAttrService as String: service,
+                kSecAttrAccount as String: tokenKey,
+                kSecReturnData as String: true,
+                kSecMatchLimit as String: kSecMatchLimitOne
+            ]
+
+            var dataTypeRef: AnyObject?
+
+            let status = SecItemCopyMatching(query as CFDictionary, &dataTypeRef)
+
+            if status == errSecSuccess, let data = dataTypeRef as? Data {
+                return String(data: data, encoding: .utf8)
             }
+
+            return nil
+        }
+
+        set {
+            // First remove any existing item
+            clearToken()
+
+            guard let token = newValue else { return }
+
+            let data = Data(token.utf8)
+            let query: [String: Any] = [
+                kSecClass as String: kSecClassGenericPassword,
+                kSecAttrService as String: service,
+                kSecAttrAccount as String: tokenKey,
+                kSecValueData as String: data
+            ]
+
+            SecItemAdd(query as CFDictionary, nil)
         }
     }
 
     static func clearToken() {
-        UserDefaults.standard.removeObject(forKey: Keys.token)
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: tokenKey
+        ]
+        SecItemDelete(query as CFDictionary)
     }
 }
