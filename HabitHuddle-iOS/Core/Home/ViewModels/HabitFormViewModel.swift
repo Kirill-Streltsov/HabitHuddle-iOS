84aa019a0bf67c2a085b494b47d062e5e3c1de03
Kirill <krlsrlsv@gmail.com>
2025-05-30 16:56:23 +0200

    Added an updateHabit method

--- HabitHuddle-iOS/Core/Home/ViewModels/HabitFormViewModel.swift
+++ HabitHuddle-iOS/Core/Home/ViewModels/HabitFormViewModel.swift
@@ -19,7 +19,7 @@ extension HabitFormView {
         @Published var reminderTime: Date = Date()
 
 
-        func sendHabit() async -> Result<CodableHabit, APIError> {
+        func createHabit() async -> Result<CodableHabit, APIError> {
             do {
                 let reminder: Date? = hasReminder ? reminderTime : nil
 
@@ -33,6 +33,32 @@ extension HabitFormView {
                 let habitResponse = try await NetworkingManager.shared.request(
                     endpoint: .createHabit(),
                     method: .post,
+                    body: payload,
+                    responseType: CodableHabit.self
+                )
+                return .success(habitResponse)
+
+            } catch let error as APIError {
+                return .failure(error)
+            } catch {
+                return .failure(.unknown)
+            }
+        }
+        
+        func updateHabit(habitID: UUID) async -> Result<CodableHabit, APIError> {
+            do {
+                let reminder: Date? = hasReminder ? reminderTime : nil
+
+                let payload = HabitPayload(
+                    name: name,
+                    description: description.isEmpty ? nil : description,
+                    frequency: frequency.rawValue,
+                    reminderTime: reminder
+                )
+
+                let habitResponse = try await NetworkingManager.shared.request(
+                    endpoint: .updateHabit(habitID: habitID),
+                    method: .put,
                     body: payload,
                     responseType: CodableHabit.self
                 )
