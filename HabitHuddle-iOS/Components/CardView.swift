e03ff449fa69c7038756a3e2d9aab43a3a11077d
Kirill <krlsrlsv@gmail.com>
2025-05-31 23:58:34 +0200

    Brought the old background color back

--- HabitHuddle-iOS/Components/CardView.swift
+++ HabitHuddle-iOS/Components/CardView.swift
@@ -13,9 +13,9 @@ struct CardView<Content: View>: View {
     var body: some View {
         content()
             .padding()
-            .background(.black.opacity(0.03))
+            .background(Color(.systemBackground))
             .cornerRadius(16)
-            //.shadow(color: .black.opacity(0.1), radius: 8, x: 0, y: 4)
+            .shadow(color: .black.opacity(0.1), radius: 8, x: 0, y: 4)
             .padding(.horizontal)
     }
 }
