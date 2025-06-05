e639e7867ba0329c1e31e79f788da7c0d92da4c3
Kirill <krlsrlsv@gmail.com>
2025-06-05 13:30:39 +0200

    Removed the empty line

--- HabitHuddle-iOS/Core/Home/Views/HabitDetailView.swift
+++ HabitHuddle-iOS/Core/Home/Views/HabitDetailView.swift
@@ -204,7 +204,6 @@ struct HabitDetailView: View {
                 handleResult(result) { codableHabit in
                     print("✅ Deleted the habit on the server with habit name: '\(codableHabit.name)' and id: '\(codableHabit.id)'")
                 } onFailure: { error in
-
                     //SyncManager.shared.add(SyncOperation(habitID: habit.id, action: .delete))
                     print("❌ Failed to delete the habit on the server: \(error.localizedDescription)")
                 }
