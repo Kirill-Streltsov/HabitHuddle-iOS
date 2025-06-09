e521fe3042cd0c9f359e5764c19d7f2a727b51e4
Kirill <krlsrlsv@gmail.com>
2025-06-09 12:12:10 +0200

    Changed logic for the checkIn

--- HabitHuddle-iOS/Model/Local Persistence/Habit+checkIn.swift
+++ HabitHuddle-iOS/Model/Local Persistence/Habit+checkIn.swift
@@ -20,10 +20,9 @@ extension Habit {
 
     func toggleCheckIn(in context: ModelContext) {
         let calendar = Calendar.current
-        let finishDate = calendar.date(byAdding: .day, value: self.duration.numberOfDays, to: self.createdAt)
         let today = calendar.startOfDay(for: .now)
         
-        if let finish = finishDate, today <= finish {
+        if checkIns.count < duration.numberOfDays {
             if let existingCheckIn = checkIns.first(where: { Calendar.current.isDate($0.date, inSameDayAs: today) }) {
                 context.delete(existingCheckIn)
             } else {
