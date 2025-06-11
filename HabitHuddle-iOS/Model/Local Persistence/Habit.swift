c11ab817a31c1f7186f0a53344b0e8a2d7982128
Kirill <krlsrlsv@gmail.com>
2025-06-11 17:41:53 +0200

    Removed a comment

--- HabitHuddle-iOS/Model/Local Persistence/Habit.swift
+++ HabitHuddle-iOS/Model/Local Persistence/Habit.swift
@@ -137,7 +137,7 @@ extension Habit {
 
         // Check-in days (relative to today), including a recent 6-day streak
         let offsets: [Int] = [
-            20, 18, 15, 12, 10, 8, // earlier scattered
+            20, 18, 15, 12, 10, 8,
             5, 4, 3, 2, 1, 0,
         ]
 
