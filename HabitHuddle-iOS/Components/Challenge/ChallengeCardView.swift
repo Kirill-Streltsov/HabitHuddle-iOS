50885eb593ae62468e67230e6a80d5a7dc52d138
Kirill <krlsrlsv@gmail.com>
2025-06-21 22:27:08 +0200

    Added a line

--- HabitHuddle-iOS/Components/Challenge/ChallengeCardView.swift
+++ HabitHuddle-iOS/Components/Challenge/ChallengeCardView.swift
@@ -48,6 +48,7 @@ struct ChallengeCardView: View {
             }
             .foregroundStyle(.secondary)
 
+            
             HStack {
                 Button {
                     Task {
