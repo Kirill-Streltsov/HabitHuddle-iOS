b00585442475ff338272c32d588b8ef69bb660cf
Kirill <krlsrlsv@gmail.com>
2025-06-24 15:01:23 +0200

    Can now call the app icon from code

---
+++ HabitHuddle-iOS/Helpers/Bundle+icon.swift
@@ -0,0 +1,19 @@
+//
+//  Bundle+icon.swift
+//  HabitHuddle-iOS
+//
+//  Created by Kirill on 24.06.25.
+//
+
+import UIKit
+extension Bundle {
+    public var icon: UIImage? {
+        if let icons = infoDictionary?["CFBundleIcons"] as? [String: Any],
+            let primaryIcon = icons["CFBundlePrimaryIcon"] as? [String: Any],
+            let iconFiles = primaryIcon["CFBundleIconFiles"] as? [String],
+            let lastIcon = iconFiles.last {
+            return UIImage(named: lastIcon)
+        }
+        return nil
+    }
+}
