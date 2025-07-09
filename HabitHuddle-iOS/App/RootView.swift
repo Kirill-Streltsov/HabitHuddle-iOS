d8529aac69a6d267009ac9fc4fee7c98d0f9e6f9
Kirill <krlsrlsv@gmail.com>
2025-07-09 21:25:57 +0200

    Removed a tab

--- HabitHuddle-iOS/App/RootView.swift
+++ HabitHuddle-iOS/App/RootView.swift
@@ -21,7 +21,7 @@ struct RootView: View {
     @EnvironmentObject private var userManager: LocalUserManager
     
     @State private var showLoggedOut = false
-                
+
     @Query
     var habits: [Habit]
 
