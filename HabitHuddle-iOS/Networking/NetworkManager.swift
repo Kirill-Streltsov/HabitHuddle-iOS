8c3c3dcf2fb9fe7a5c2c9c0bf55b5aa86683bb5a
Kirill <krlsrlsv@gmail.com>
2025-08-12 21:25:54 +0200

    Added a line

--- HabitHuddle-iOS/Networking/NetworkManager.swift
+++ HabitHuddle-iOS/Networking/NetworkManager.swift
@@ -163,6 +163,7 @@ actor NetworkManager {
     
     
     
+    
     private func handleResponse<T: Decodable>(data: Data, response: HTTPURLResponse, responseType _: T.Type) throws -> T {
         switch response.statusCode {
         case 200 ..< 300:
